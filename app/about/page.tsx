5680dca40025f643d691d11d84c260899e6f6c0b
nachatra-sharma <nakshusharm0@gmail.com>
2025-12-07 21:45:25 +0530

    fix: fixed responsive design for about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,7 +5,7 @@ export default function About() {
   return (
     <div className="relative flex w-full flex-col items-start justify-start overflow-hidden">
       <NavbarComponent />
-      <div className="mt-30 max-w-[600px] mx-auto">
+      <div className="mt-20 sm:mt-30 max-w-[330px] sm:max-w-[600px] mx-auto">
         <AboutComponent />
       </div>
     </div>
