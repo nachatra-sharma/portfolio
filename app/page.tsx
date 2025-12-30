34f1d08478e24bccceaad2401f450eb2eb951e51
nachatra-sharma <nakshusharm0@gmail.com>
2025-12-30 18:14:02 +0530

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import AboutComponent from "@/components/custom/about/AboutComponent";
+import { NavbarComponent } from "@/components/custom/Navbar";
 
 export default function Home() {
   return (
-    <div className="mt-20 sm:mt-30 max-w-[330px] sm:max-w-[600px] mx-auto">
-      <AboutComponent />
+    <div className="relative flex w-full flex-col items-start justify-start overflow-hidden">
+      <NavbarComponent />
+      <div className="mt-20 sm:mt-30 max-w-[330px] sm:max-w-[600px] mx-auto">
+        <AboutComponent />
+      </div>
     </div>
   );
 }
