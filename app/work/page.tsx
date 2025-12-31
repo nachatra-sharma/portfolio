f04f3d9b2747666631c366886d994992404586f8
nachatra-sharma <nakshusharm0@gmail.com>
2025-12-31 07:51:08 +0530

    add: added work page

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -1,3 +1,4 @@
+import { FocusCards } from "@/components/ui/focus-cards";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -5,5 +6,28 @@ export const metadata: Metadata = {
 };
 
 export default function Work() {
-  return <></>;
+  const cards = [
+    {
+      title: "Atwatch Mobile App",
+      src: "/work/atwatch_mobile.png",
+      link: "https://play.google.com/store/apps/details?id=com.prnkinfotech.atwatch",
+    },
+    {
+      title: "Atwatch Admin",
+      src: "/work/atwatch_admin.png",
+      link: "https://atwatch.in/",
+    },
+    {
+      title: "iWonder",
+      src: "/work/iwonder.png",
+      link: "https://iwonder.com/",
+    },
+    {
+      title: "HelloCalc",
+      src: "/work/hellocalc.png",
+      link: "https://opensupportsuite.com/home",
+    },
+  ];
+
+  return <FocusCards cards={cards} />;
 }
